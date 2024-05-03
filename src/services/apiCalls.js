a6af27587295998d4d2817b184e9c2b482c8ad90
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-05-03 10:07:05 +0200

    feat: Add function Login to apiCalls

---
+++ src/services/apiCalls.js
@@ -0,0 +1,21 @@
+const apiUrl = "http://localhost:4000"
+
+export const login = async (bodyCredentials) => {
+    try {
+        const response = await fetch(
+            `${apiUrl}/api/auth/login`,
+            {
+                method: "Post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(bodyCredentials),
+            }
+        )
+        const data = await response.json()
+
+        return data
+    } catch (error) {
+        return error
+    }
+}
