c92e2a48610bcca801effe0188f1f28f57350119
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-05-03 10:02:14 +0200

    Feat: add Button and Input components

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,128 @@
 
+import React, { useState } from 'react'
+import { Input } from '../../common/Input/Input'
 import "./Login.css"
+import { login } from '../../services/apiCalls'
+import { useNavigate } from 'react-router-dom'
+import { decodeToken } from 'react-jwt'
+import { Button } from '../../common/Button/Button'
 
-import React from 'react'
+import { useDispatch } from "react-redux";
+import { loginRdx } from "../../app/slices/userSlice";
+
 
 export const Login = () => {
+  //Instancia de Redux para escritura
+  const dispatch = useDispatch();
+
+  const [bodyCredentials, setBodyCredentials] = useState(
+    {
+      email: "",
+      password: ""
+    }
+  )
+
+  const [bodyCredentialsError, setBodyCredentialsError] = useState(
+    {
+      email: "",
+      password: ""
+    }
+  )
+
+  const navigate = useNavigate()
+
+  const LogMe = async () => {
+    setBodyCredentialsError({...bodyCredentialsError, email: ""})
+    setBodyCredentialsError({...bodyCredentialsError, password: ""})
+
+    console.log(1);
+    if(!bodyCredentials.email) {
+      setBodyCredentialsError({...bodyCredentialsError, email: "Introduce un email"})
+
+      return;
+    }
+
+    if (bodyCredentials.password === "") {
+      console.log(3);
+      setBodyCredentialsError({...bodyCredentialsError, password: "Introduce el password"})
+      return;
+    }
+console.log(4);
+    // Llamar a la función de inicio de sesión proporcionando las credenciales
+    const responseApiLogin = await login(bodyCredentials)
+    const decoded = decodeToken(responseApiLogin.token)
+
+    console.log(decoded);
+    // Si el inicio de sesión es exitoso y el usuario tiene rol user
+    if (responseApiLogin.success && decoded.roleName === "user") {
+      dispatch(loginRdx({
+        token: responseApiLogin.token,
+        userId: decoded.userId,
+        // name: decoded.username,
+        role: decoded.roleName
+      }))
+
+      navigate("/home")
+    } else {
+      // Si el inicio de sesión es exitoso y el usuario es admin o superadmin
+      dispatch(loginRdx({
+          token: responseApiLogin.token,
+          userId: decoded.userId,
+          // name: decoded.username,
+          role: decoded.roleName
+      }))
+
+      navigate("/home")
+    }
+  }
+
+  const inputHandler = (e) => {
+    setBodyCredentials((prevState) => (
+      {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    ))
+
+  }
   return (
-    <div>Login</div>
+    <>
+      <div className='loginDesign'>
+        <div className="formLogin">
+          <h5> Accede a tu cuenta</h5>
+          <Input
+            className="inputDesign"
+            type="email"
+            placeHolder="email"
+            name="email"
+            onChangeFunction={(e) => inputHandler(e)}
+          />
+          {
+            bodyCredentialsError.email === "Introduce un email" 
+              ?  <div> { bodyCredentialsError.email } </div> 
+              :  <div></div>     
+          }
+          <Input
+            className="inputDesign"
+            type="password"
+            placeHolder="password"
+            name="password"
+            onChangeFunction={(e) => inputHandler(e)}
+          />
+          {
+            bodyCredentialsError.password === "Introduce el password" 
+              ?  <div> { bodyCredentialsError.password } </div> 
+              :  <div></div>     
+          }
+
+          <Button
+            title={"Entrar"}
+            className="ButtonDesign"
+            onClick={LogMe}
+          />
+
+        </div>
+      </div>
+    </>
   )
 }
