a9f135d16bc84eebc9a0696bdbfacf10dee1e9bc
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-05-06 15:04:23 +0200

    Feat:Add Google Fonts stylesheet and update background color in Header and Button components

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import { Login } from "../Login/Login"
 import { Register } from "../Register/Register"
+import { CreateIssue } from "../createIssue/CreateIssue"
 
 export const Body = () => {
     return (
         <Routes>
              <Route path="/login" element={<Login/>} />
              <Route path="/register" element={<Register/>} />
+             <Route path="/create-issue" element={<CreateIssue />} />
         </Routes>
     )
 }
---
+++ src/pages/createIssue/CreateIssue.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import "./CreateIssue.css"
+import { Button } from '../../common/Button/Button'
+import { Input } from '../../common/Input/Input'
+
+export const CreateIssue = () => {
+    const [bodyDataIssue, setBodyDataIssue] = useState(
+        {
+            title: '',
+            issueTypeId: '',
+            departamentId: '',
+            description: '',
+        }
+    )
+
+    const inputHandler = (e) => {
+        setBodyDataIssue((prevState) => (
+            {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        ))
+    }
+
+
+    return (
+
+        <div className='create-issue-design'>
+            {/* <h2>Crear Incidencia</h2> */}
+            <div className="container-create-issue">
+
+                
+                <div className='container-issue-1 container-issue'>
+                <div className="form-group">
+                    <label>Tipo de Incidencia</label>
+                    <input
+                        type="text"
+                        className="input-issue-design"
+                        name="issueTypeId"
+                        onChange={(e) => inputHandler(e)} />
+                </div>
+                <div className="form-group">
+                    <label>Departamento</label>
+                    <input
+                        type="text"
+                        className="input-issue-design"
+                        name="departamentId"
+                        onChange={(e) => inputHandler(e)} />
+                </div>
+                </div>
+                <div className='container-issue-2 container-issue'>
+                <div className="form-group">
+                    <label>Titulo</label>
+                    <Input
+
+                        type="text"
+                        className="input-issue-design"
+                        name="title"
+                        onChange={(e) => inputHandler(e)} />
+                </div>
+                <div className="form-group">
+                    <label>Descripción</label>
+                    <textarea
+                        className="input-issue-design"
+                        name="description"
+                        onChange={(e) => inputHandler(e)} />
+                </div>
+                </div>
+                <div className='container-issue-3'>
+
+                <Button
+                        title={"Crear Incidencia"}
+                        className="ButtonDesign"
+                        // onClick={LogMe}
+                    />
+
+                </div>
+            </div>
+        </div>
+    )
+}
